002782fbe85ca4f76338588c0e2bf9bbed4cfd54
NoCapAAAAA <arttyyom@yandex.ru>
2023-04-27 18:48:50 +0300

    aa

--- scr/organization/urls.py
+++ scr/organization/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('director/', director_views.DirectorHomeView.as_view(), name='director_home'),
     path('director/user-list/', director_views.DirectorUsersListView.as_view(), name='director_create_employee'),
     path('director/user-list/<int:pk>/', director_views.DirectorUpdateUserView.as_view(), name='director_get_user_permissions'),
-    path('director/report-orders', director_views.DirectorOrdersReportView.as_view(), name='director_orders_report_view'),
-    path('director/report-users', director_views.DirectorUsersReportView.as_view(), name='director_users_report_view'),
+    path('director/report-orders/', director_views.TestDocument, name='director_orders_report_view'),
+    path('director/report-users/', director_views.DirectorUsersReportView.as_view(), name='director_users_report_view'),
     path('director/list-empl/', director_views.DirectorListEmployeeView.as_view(), name='director_list_employee_view'),
     path('director/list-empl/<int:pk>/', director_views.DirectorEmployeeDetailView.as_view(), name='director_detail_employee_view'),
 
@@ -16,40 +16,11 @@ urlpatterns = [
 
     # Менеджер
     path('manager/', manager_views.ManagerHomeView.as_view(), name='manager_home'),
-
     path('manager/order-crete', manager_views.ManagerCreateOrderView.as_view(), name='manager_create_order_view'),
     path('manager/order-list/', manager_views.ManagerOrdersListView.as_view(), name='manager_orders_list_view'),
     path('manager/order-list/<int:pk>/', manager_views.ManagerDetailOrderView.as_view(), name='manager_detail_order_view'),
-
     path('manager/create-user', manager_views.ManagerCreateUserView.as_view(), name='manager_create_user_view'),
     path('manager/user-list/', manager_views.ManagerUsersListView.as_view(), name='manager_users_list_view'),
     path('manager/user-list/<int:pk>/', manager_views.ManagerDetailUserView.as_view(), name='manager_detail_user_view'),
-
-    # Создание контрагента
-    # Создание отчёта по пользователям
-    # Создание отчёта по заказам
-
-    # path('director/users', v.DirectorUsersView.as_view(), name='directorUsers'),
-    # path('director/clients', v.DirectorClientsView.as_view(), name='directorClients'),
-    # path('director/stuff', v.DirectorStuffView.as_view(), name='directorStuff'),
-    # path('director/orders/', v.DirectorOrdersView.as_view(), name='directorOrders'),
-    # path('order_create/', v.CreateOrder.as_view(), name='create_order'),
-    # path('director/orders/<int:pk>/', v.DetailOrderViewDir.as_view(), name='directorOrdersDetail'),
-    #
-    # path('director/create_user/', v.CreateUser.as_view(), name='create_user'),
-    # path('director/statistics', v.DirectorStatisticsView.as_view(), name='directorStatistics'),
-    # # path('docs/', views.TestDocument, name='report_download'),
-    #
-    # path('manager/', v.ManagerHomeView.as_view(), name='managerHome'),
-    # path('setting/', v.ManagerEdit.as_view(), name='manager_edit'),
-    # path('manager/pass/', v.PassChange.as_view(), name='pass_change'),
-
-    # Home
-    # Создание (Заказа, Пользователяб)
-    # Список заказов
-    # Список пользователей
-    # Детальный просмотр заказа(и его редактирование)
-    # Детальный просмотр пользователя(и его редактирование)
-    #
 
 ]
--- scr/organization/views/director.py
+++ scr/organization/views/director.py
@@ -1,26 +1,23 @@
-import time
-from core.models import OrderStorage
+from core import models as m
 from django.contrib import messages
 from service.filters import UsersFilterDirector
 from django.contrib.auth.models import Group
 from organization import forms as f
 from django.utils.decorators import method_decorator
-from django.views.generic import UpdateView, CreateView, TemplateView, ListView
-from django.contrib.auth import get_user_model, authenticate
+from django.views.generic import UpdateView,ListView
 from django.urls import reverse_lazy
 from organization.forms import CreateEmployeeForm
 from service.decorators import group_required
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django.views.generic import TemplateView
-from django.http import JsonResponse
 from django.db.models.functions import ExtractYear, ExtractMonth
 from django.db.models import Count
 from django.http import HttpResponse
-from docx import Document
+from docx import *
+from io import BytesIO
 from datetime import datetime, date
 import locale
-from io import BytesIO
+
 User = get_user_model()
 
 
@@ -28,13 +25,15 @@ class DirectorHomeView(TemplateView):
     @method_decorator(group_required('Директор'))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
     template_name = 'director/home_director.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         # Get filter options
-        grouped_purchases = OrderStorage.objects.annotate(year=ExtractYear("created_at")).values("year").order_by("-year").distinct()
+        grouped_purchases = m.OrderStorage.objects.annotate(year=ExtractYear("created_at")).values("year").order_by(
+            "-year").distinct()
         options = [purchase["year"] for purchase in grouped_purchases]
         context["filter_options"] = options
 
@@ -42,10 +41,11 @@ class DirectorHomeView(TemplateView):
         year = self.request.GET.get("year")
         if year:
             purchases = Purchase.objects.filter(time__year=year)
-            grouped_purchases = purchases.annotate(month=ExtractMonth("created_at")).values("month").annotate(count=Count("id")).values("month", "count").order_by("month")
+            grouped_purchases = purchases.annotate(month=ExtractMonth("created_at")).values("month").annotate(
+                count=Count("id")).values("month", "count").order_by("month")
             sales_dict = get_year_dict()
             for group in grouped_purchases:
-                sales_dict[months[group["month"]-1]] = group["count"]
+                sales_dict[months[group["month"] - 1]] = group["count"]
             context["sales_data"] = {
                 "title": f"Sales in {year}",
                 "data": {
@@ -66,6 +66,7 @@ class DirectorUsersListView(ListView):
     @method_decorator(group_required('Директор'))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
     template_name = 'director/users_list_director.html'
     model = User
     paginate_by = 7
@@ -86,6 +87,7 @@ class DirectorUpdateUserView(UpdateView):
     @method_decorator(group_required('Директор'))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
     template_name = 'director/user_get_permissions.html'
     model = User
     form_class = f.SettingsProfile
@@ -105,6 +107,7 @@ class DirectorEmployeeDetailView(UpdateView):
     @method_decorator(group_required('Директор'))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
     template_name = 'director/detail_employee_director.html'
     model = User
     form_class = f.SettingsProfile
@@ -137,6 +140,7 @@ class DirectorListEmployeeView(ListView):
     @method_decorator(group_required('Директор'))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
     template_name = 'director/list_employee_director.html'
     model = User
     paginate_by = 7
@@ -144,95 +148,13 @@ class DirectorListEmployeeView(ListView):
     queryset = User.objects.filter(groups__name='Менеджер')
 
 
-class DirectorOrdersReportView(TemplateView):
-    @method_decorator(group_required('Директор'))
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-    template_name = 'director/orders_report_director.html'
-    model = OrderStorage
-
-    def get(self, request, *args, **kwargs):
-        date_from = request.GET.get('date_from')
-        date_until = request.GET.get('date_until')
-        records = OrderStorage.objects.filter(created_at__range=[date_from, date_until])
-
-        return super().get(request, *args, **kwargs, records=records)
-
-    def gen_report_(self):
-        locale.setlocale(
-            category=locale.LC_ALL,
-            locale="Russian"  # Note: do not use "de_DE" as it doesn't work
-        )
-        current_datetime = datetime.now()
-        str_current_datetime = str(current_datetime)
-        document = Document()
-        docx_title = "report" + str_current_datetime + ".docx"
-        # ---- Cover Letter ----
-        document.add_paragraph()
-        document.add_paragraph("%s" % date.today().strftime('%B %d, %Y'))
-
-        document.add_paragraph('Отчёт о заказах')
-        qs2 = self.records.count()
-        qs2count = qs2.count() + 1
-        table = document.add_table(rows=qs2count, cols=8)
-        table.style = 'Table Grid'
-        table.cell(0, 0).text = 'Номер заказа'
-        table.cell(0, 1).text = 'Клиент'
-        table.cell(0, 2).text = 'Размер шины'
-        table.cell(0, 3).text = 'Период хранения'
-        table.cell(0, 4).text = 'Адрес сервиса'
-        table.cell(0, 5).text = 'Статус заказа'
-        table.cell(0, 6).text = 'Стоимость заказа'
-        table.cell(0, 7).text = 'Оплачен'
-
-        # Creating a table object
-
-        for order in records:
-
-            qs2 = m.OrderStorage.objects.all().order_by('pk')
-            row = qs2.count()
-            table.cell(row, 0).text = str(order.pk)
-            table.cell(row, 1).text = str(order.user)
-            table.cell(row, 2).text = str(order.size)
-            table.cell(row, 3).text = str(order.period)
-            if order.adress == None:
-                order.adress = "---"
-            table.cell(row, 4).text = str(order.adress)
-            table.cell(row, 5).text = str(order.get_status_display())
-            table.cell(row, 6).text = str(order.price)
-            if order.is_payed == True:
-                order.is_payed = "Да"
-            else:
-                order.is_payed = "Нет"
-            table.cell(row, 7).text = str(order.is_payed)
-
-        document.add_page_break()
-
-        # Prepare document for download
-        # -----------------------------
-        f = BytesIO()
-        document.save(f)
-        length = f.tell()
-        f.seek(0)
-        response = HttpResponse(
-            f.getvalue(),
-            content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        )
-        response['Content-Disposition'] = 'attachment; filename=' + docx_title
-        response['Content-Length'] = length
-        return response
-
-
-
-
-
-
 class DirectorUsersReportView(TemplateView):
     @method_decorator(group_required('Директор'))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
     template_name = 'director/users_report_director.html'
-##
+
 def TestDocument(request):
     locale.setlocale(
         category=locale.LC_ALL,
@@ -296,5 +218,3 @@ def TestDocument(request):
     response['Content-Disposition'] = 'attachment; filename=' + docx_title
     response['Content-Length'] = length
     return response
-
-
